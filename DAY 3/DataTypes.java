a2d0f579b0a2451688304830bac72cf81d3eab53
pavan347 <gktechspheres@gmail.com>
2024-10-30 14:54:27 +0530

    images are added

---
+++ DAY 3/DataTypes.java
@@ -0,0 +1,16 @@
+class DataTypes {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Value of a: " + a);
+        float b = 10.5f;
+        System.out.println("Value of b: " + b);
+        double c = 10.5;
+        System.out.println("Value of c: " + c);
+        char d = 'A';
+        System.out.println("Value of d: " + d);
+        boolean e = true;
+        System.out.println("Value of e: " + e);
+        String f = "Hello";
+        System.out.println("Value of f: " + f);
+    }
+}
